8637296dbac6da522cdb0c4f3a7832541bc1d51b
ValentinSILVESTRE <valentin.silvestre@hotmail.com>
2022-12-13 16:10:01 +0100

    routing

--- api-node/src/controllers/controllers.ts
+++ api-node/src/controllers/controllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { connect } from 'mongoose';
 import Absence from '../models/Absence';
 
-const dbURI = 'mongodb://localhost:27017/absenceApp';
+const dbURI = 'mongodb://127.0.0.1:27017/absenceApp';
 connect(dbURI);
 const getAll = () => {
 	const absences = Absence.find({});
--- api-node/src/index.ts
+++ api-node/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import router from './routes/routes';
 
 const app = express();
 const port = 3000;
+app.use(router);
 
 app.listen(port, () => {
 	console.log(`Connected at port ${port}`);
