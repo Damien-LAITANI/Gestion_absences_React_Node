c82958a8f2d6c9294ebe9b8b0088df0577973f4f
Damien Laitani <d.laitani@gmail.com>
2022-12-13 17:14:54 +0100

    test creating user

--- api-node/src/controllers/controllers.ts
+++ api-node/src/controllers/controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { connect } from 'mongoose';
 import Absence from '../models/Absence';
+import User from '../models/User';
 
 const dbURI = 'mongodb://127.0.0.1:27017/absenceApp';
 connect(dbURI);
@@ -10,16 +11,25 @@ const getAll = () => {
 };
 
 export const postOne = (req: Request, res: Response) => {
-	const absence = new Absence({
+	const absence = {
 		startDate: Date.now(),
 		endDate: Date.now(),
 		types: 'congé payé',
 		motif: 'string',
 		status: 'REJETEE',
+	};
+
+	const user = new User({
+		firstname: 'test',
+		lastname: 'test',
+		email: 'email',
+		password: 'password',
+		roles: 'admin',
+		absences: [absence],
 	});
 
-	Absence.create(absence, (error) => {
-		console.log(absence);
+	User.create(user, (error) => {
+		console.log(user);
 		console.log(error);
 		res.end();
 	});
--- api-node/src/routes/routes.ts
+++ api-node/src/routes/routes.ts
@@ -3,6 +3,6 @@ import { postOne } from '../controllers/controllers';
 
 const router = Router();
 
-router.get('/absence', postOne);
+router.get('/user', postOne);
 
 export default router;
