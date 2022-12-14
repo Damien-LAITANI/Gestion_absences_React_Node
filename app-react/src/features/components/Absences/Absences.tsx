078c16103a6a5d64b9d7a32540272b26de861515
Damien Laitani <d.laitani@gmail.com>
2022-12-14 17:23:15 +0100

    Ajout du composant Holiday

--- app-react/src/features/components/Absences/Absences.tsx
+++ app-react/src/features/components/Absences/Absences.tsx
@@ -1,13 +1,11 @@
 import AbsenceForm from '../AbsenceForm/AbsenceForm';
 import AbsenceList from '../AbsenceList/AbsenceList';
-import AbsenceModal from '../AbsenceModal/AbsenceModal';
 
 const Absences = () => {
 	return (
 		<>
 			<AbsenceList />
 			<AbsenceForm />
-			<AbsenceModal />
 		</>
 	);
 };
