b1daa9fe738436c85a2b4012ac07e028b26505df
Damien Laitani <d.laitani@gmail.com>
2022-12-14 10:48:16 +0100

    add routes GET, PUT, POST, DELETE

--- api-node/src/controllers/controllers.ts
+++ api-node/src/controllers/controllers.ts
@@ -5,9 +5,34 @@ import User from '../models/User';
 
 const dbURI = 'mongodb://127.0.0.1:27017/absenceApp';
 connect(dbURI);
-const getAll = () => {
-	const absences = Absence.find({});
-	console.log();
+
+export const getAllUser = (req: Request, res: Response) => {
+	const users = User.find({});
+	res.status(200).json(users);
+};
+
+export const getUser = async (req: Request, res: Response) => {
+	await User.findById(req.params._id, (error: any, user: typeof User) => {
+		res.status(200).json(user);
+	});
+};
+
+export const addUser = async (req: Request, res: Response) => {
+	await User.create(req.body, (error: any, user: typeof User) => {
+		res.status(201).json(user);
+	});
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+	const updatedUser = req.body;
+	const user = await User.replaceOne({ _id: req.body._id }, updatedUser);
+	res.status(200).json(user);
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+	const { _id } = req.params;
+	const user = await User.deleteOne({ _id: _id });
+	res.status(200).json(user);
 };
 
 export const postOne = (req: Request, res: Response) => {
--- api-node/src/routes/routes.ts
+++ api-node/src/routes/routes.ts
@@ -1,8 +1,18 @@
 import { Router } from 'express';
-import { postOne } from '../controllers/controllers';
+import {
+	getAllUser,
+	getUser,
+	addUser,
+	updateUser,
+	deleteUser,
+} from '../controllers/controllers';
 
 const router = Router();
 
-router.get('/user', postOne);
+router.get('/user', getAllUser);
+router.get('/user/:id', getUser);
+router.post('/user/', addUser);
+router.put('/user/', updateUser);
+router.delete('/user/:id', deleteUser);
 
 export default router;
