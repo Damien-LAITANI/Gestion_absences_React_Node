5c63abb7fd9b1827ca8567a323d969f19375559d
ValentinSILVESTRE <valentin.silvestre@hotmail.com>
2022-12-15 17:36:37 +0100

    Affichage et formulaire des jours fériés - incomplet

--- app-react/src/features/components/Holiday/Holiday.tsx
+++ app-react/src/features/components/Holiday/Holiday.tsx
@@ -2,14 +2,27 @@ import { useState } from 'react';
 import HolidayForm from '../HolidayForm/HolidayForm';
 import HolidayList from '../HolidayList/HolidayList';
 
+const defaultHoliday = {
+	date: '30/10/2020',
+	type: 'RTT',
+	jour: 'Lundi',
+	commentaires: 'Rien',
+};
+
 const Holiday = () => {
 	const [showHolidayForm, setShowHolidayForm] = useState<Boolean>(false);
+	const [holidays, setHolidays] = useState([defaultHoliday, defaultHoliday]);
+
 	return (
 		<>
 			{showHolidayForm ? (
 				<HolidayForm setShowHolidayForm={setShowHolidayForm} />
 			) : (
-				<HolidayList setShowHolidayForm={setShowHolidayForm} />
+				<HolidayList
+					holidays={holidays}
+					setHolidays={setHolidays}
+					setShowHolidayForm={setShowHolidayForm}
+				/>
 			)}
 		</>
 	);
