78478ce6c10ad71434955b04787ebe9b84f48eaf
Damien Laitani <d.laitani@gmail.com>
2022-12-15 17:17:38 +0100

    ajout du toggle pour visualiser le mot de passe

--- app-react/src/features/components/Login/Login.tsx
+++ app-react/src/features/components/Login/Login.tsx
@@ -1,12 +1,16 @@
 import { login } from '../../../services/connectService';
 import { useNavigate } from 'react-router-dom';
+import './Login.scss';
+import { useState } from 'react';
 
 interface ILogin {
 	setUser: Function;
 }
 
 const Login = ({ setUser }: ILogin) => {
+	const [showPassword, setShowPassword] = useState(false);
 	const navigate = useNavigate();
+
 	const handleConnection = async (event: any) => {
 		event.preventDefault();
 		const [email, password] = event.target;
@@ -24,40 +28,75 @@ const Login = ({ setUser }: ILogin) => {
 		console.log(response);
 	};
 
+	const handleDisplayPass = (event: any) => {
+		const inputPassword: Element | null =
+			document.querySelector('#password');
+
+		setShowPassword(!showPassword);
+		if (!showPassword) {
+			return inputPassword?.setAttribute('type', 'text');
+		}
+		inputPassword?.setAttribute('type', 'password');
+	};
+
 	return (
-		<>
-			<img src="" alt="logo" />
-			<h1>Respire</h1>
-			<h2>Gestion des absences</h2>
+		<div className="container__login w-50 mx-auto d-flex flex-column align-items-center">
+			<img src="favicon.png" alt="logo" />
+			<h1 className="app__name">Respire</h1>
+			<h2 className="app__sub">Gestion des absences</h2>
 			<form onSubmit={handleConnection}>
-				<div className="mb-3">
-					<label htmlFor="email" className="form-label">
-						Email
-					</label>
+				<div className="form-floating mb-3">
 					<input
 						type="text"
 						name="email"
 						className="form-control"
 						id="email"
 						aria-describedby="emailHelp"
+						placeholder="Email"
 					/>
+					<label htmlFor="email">Email</label>
 				</div>
-				<div className="mb-3">
-					<label htmlFor="password" className="form-label">
-						Mot de passe
-					</label>
+				<div className="mb-3 d-flex justify-content-between align-items-center">
 					<input
 						type="password"
 						name="password"
 						className="form-control"
+						placeholder="Password"
 						id="password"
 					/>
+					{showPassword ? (
+						<svg
+							onClick={handleDisplayPass}
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							className="showPass bi bi-eye-slash-fill"
+							viewBox="0 0 16 16"
+						>
+							<path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+							<path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z" />
+						</svg>
+					) : (
+						<svg
+							onClick={handleDisplayPass}
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							fill="currentColor"
+							className="showPass bi bi-eye-fill"
+							viewBox="0 0 16 16"
+						>
+							<path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+							<path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+						</svg>
+					)}
 				</div>
 				<button type="submit" className="btn btn-success">
 					Se connecter
 				</button>
 			</form>
-		</>
+		</div>
 	);
 };
 
