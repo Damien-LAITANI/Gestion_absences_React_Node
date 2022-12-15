07c6b169adee78de72977fe7cd5df67643859306
ValentinSILVESTRE <valentin.silvestre@hotmail.com>
2022-12-15 10:59:20 +0100

    Affichage amélioré des premiers composants

--- app-react/src/components/Nav/Nav.tsx
+++ app-react/src/components/Nav/Nav.tsx
@@ -2,15 +2,12 @@ import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<NavLink className="navbar-brand" to="/">
-				Logo
-			</NavLink>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light d-flex align-items-center">
 			<button
 				className="navbar-toggler"
 				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNav"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
 				aria-controls="navbarNav"
 				aria-expanded="false"
 				aria-label="Toggle navigation"
@@ -19,32 +16,41 @@ const Nav = () => {
 			</button>
 			<div className="collapse navbar-collapse" id="navbarNav">
 				<ul className="navbar-nav">
-					<li className="nav-item active">
-						<NavLink to="/" className="nav-link">
-							Accueil
+					<li className="nav-item active d-flex align-items-center">
+						<NavLink
+							to="/"
+							className="nav-link d-flex align-items-center"
+						>
+							<img
+								src="favicon.png"
+								alt="cloud"
+								className="image-fluid me-1"
+								style={{ width: '40px' }}
+							/>
+							<span>Accueil</span>
 						</NavLink>
 					</li>
-					<li className="nav-item">
+					<li className="nav-item d-flex align-items-center">
 						<NavLink to="/absences" className="nav-link">
 							Gestion des absences
 						</NavLink>
 					</li>
-					<li className="nav-item">
+					<li className="nav-item d-flex align-items-center">
 						<NavLink to="/planning" className="nav-link">
 							Planning des absences
 						</NavLink>
 					</li>
-					<li className="nav-item">
+					<li className="nav-item d-flex align-items-center">
 						<NavLink to="/absences-process" className="nav-link">
 							Validation demandes
 						</NavLink>
 					</li>
-					<li className="nav-item">
+					<li className="nav-item d-flex align-items-center">
 						<NavLink to="/report-list" className="nav-link">
 							Vues synthétiques
 						</NavLink>
 					</li>
-					<li className="nav-item">
+					<li className="nav-item d-flex align-items-center">
 						<NavLink to="/holiday" className="nav-link">
 							Jours fériés
 						</NavLink>
