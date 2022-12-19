8da431571fbc560fae3e27d5819bf33fc8476360
ValentinSILVESTRE <valentin.silvestre@hotmail.com>
2022-12-19 15:38:30 +0100

    Correction bug date.ts

--- app-react/src/functions/date.ts
+++ app-react/src/functions/date.ts
@@ -1,11 +1,18 @@
 /**
  * - Renvoie vrai si les deux dates sont le même jour
  */
-export const datesAreOnSameDay = (date1: Date, date2: Date) => {
-	console.log(date1, date2);
+export const datesAreOnSameDay = (
+	date1: Date | string,
+	date2: Date | string
+) => {
+	const d1 =
+		typeof date1 === 'string' ? new Date(date1.split('T')[0]) : date1;
+	const d2 =
+		typeof date2 === 'string' ? new Date(date2.split('T')[0]) : date2;
+
 	return (
-		date1.getFullYear() === date2.getFullYear() &&
-		date1.getMonth() === date2.getMonth() &&
-		date1.getDate() === date2.getDate()
+		d1.getFullYear() === d2.getFullYear() &&
+		d1.getMonth() === d2.getMonth() &&
+		d1.getDate() === d2.getDate()
 	);
 };
